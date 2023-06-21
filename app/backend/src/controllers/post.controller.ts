a6cd9f041210814b0cd0ad3ae0df049954bdc126
Felipe Nascimento <flpnascto@gmail.com>
2023-06-21 16:31:54 -0300

    feat: implements post controller

---
+++ app/backend/src/controllers/post.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import PostService from '../services/post.service';
+
+class PostController {
+  constructor(private service: PostService) {}
+
+  async create(req: Request, res: Response): Promise<Response | void> {
+    const { title, content } = req.body;
+    const userId = req.headers.id as string;
+    const post = await this.service.create({
+      title,
+      content,
+      authorId: parseInt(userId),
+    });
+    return res.status(201).json(post);
+  }
+}
+
+export default PostController;
