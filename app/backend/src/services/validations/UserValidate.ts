da0ba8dc1ae2afcbf6fdf43b7a2a382032d8e128
Felipe Nascimento <flpnascto@gmail.com>
2023-06-22 10:04:52 -0300

    feat: update validate new user

--- app/backend/src/services/validations/UserValidate.ts
+++ app/backend/src/services/validations/UserValidate.ts
@@ -6,11 +6,21 @@ export default class UserValidate {
     if (typeof firstName !== 'string') {
       throw new BadRequestException('First name must be a string');
     }
+    if (firstName.length < 2) {
+      throw new BadRequestException(
+        'First name must be at least 2 characters long'
+      );
+    }
   }
 
   private static validateLastName(lastName: string): void {
     if (typeof lastName !== 'string') {
       throw new BadRequestException('Last name must be a string');
+    }
+    if (lastName.length < 2) {
+      throw new BadRequestException(
+        'Last name must be at least 2 characters long'
+      );
     }
   }
 
@@ -18,11 +28,22 @@ export default class UserValidate {
     if (typeof email !== 'string') {
       throw new BadRequestException('Email must be a string');
     }
+    const emailRegex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email.toLowerCase())) {
+      throw new BadRequestException('Email is not valid');
+    }
   }
 
   private static validatePassword(password: string): void {
     if (typeof password !== 'string') {
       throw new BadRequestException('Password must be a string');
+    }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      throw new BadRequestException(
+        'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter and one number'
+      );
     }
   }
 
