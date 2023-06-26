ac905617e951641b99d44b32e3bb4a563f8d10f5
Felipe Nascimento <flpnascto@gmail.com>
2023-06-26 16:20:33 -0300

    chore: remove node_modules

--- app/backend/dist/middlewares/errorMiddleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorMiddleware = (err, _req, res, _next) => {
-    console.log('err', err);
-    const { status, message } = err;
-    res.status(status || 500).json({ message });
-};
-exports.default = errorMiddleware;
